dd75ba10897f9cb0a874848a1d5a8656499b0ba9
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2025-09-24 16:55:11 +0530

    chore: setup React TypeScript project with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+  extend: {
+    colors: {
+      brand: {
+        primary: '#4F46E5',
+        secondary: '#6366F1',
+        accent: '#EEF2FF',
+      },
+    },
   },
+}
+,
   plugins: [],
 }
